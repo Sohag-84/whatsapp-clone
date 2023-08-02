ea0ae390916224b35787e355968f8fe979797218
Sohag <ihsohag-84@gmail.com>
2023-08-02 12:45:06 +0600

    pick image from gallery

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,11 +1,86 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class UserInformationScreen extends StatelessWidget {
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
+
+class UserInformationScreen extends StatefulWidget {
   static const routeName = '/user-information';
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  File? image;
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context: context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  image == null
+                      ? CircleAvatar(
+                          radius: 64,
+                          backgroundImage: NetworkImage(
+                            "https://gweb-research-imagen.web.app/compositional/An%20oil%20painting%20of%20a%20British%20Shorthair%20cat%20wearing%20a%20cowboy%20hat%20and%20red%20shirt%20skateboarding%20on%20a%20beach./1_.jpeg",
+                          ),
+                        )
+                      : CircleAvatar(
+                          radius: 64,
+                          backgroundImage: FileImage(image!),
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    right: -10,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: Icon(Icons.add_a_photo, size: 30),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: "Enter your name",
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.done),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
